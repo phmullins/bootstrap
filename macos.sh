bd0173487e7c98d59d9db1ec6d4cfa648d4656a6
Patrick H. Mullins <pmullins11@mac.com>
2016-10-29 16:08:42 -0400

    Fixed error with includes

--- macos.sh
+++ macos.sh
@@ -14,21 +14,21 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 clear
 
 # Included Modules
-defaults/bootstrap_apps.sh
-defaults/bootstrap_appstore.sh
-defaults/bootstrap_dbug.sh
-defaults/bootstrap_dock.sh
-# defaults/bootstrap_finder.sh
-# defaults/bootstrap_graphics.sh
-# defaults/bootstrap_keyboard.sh
-# defaults/bootstrap_mail.sh
-# defaults/bootstrap_mouse.sh
-# defaults/bootstrap_safari.sh
-# defaults/bootstrap_spaces.sh
-# defaults/bootstrap_system.sh
-# defaults/bootstrap_textedit.sh
-# defaults/bootstrap_tmachine.sh
-# defaults/bootstrap_trackpad.sh
+sh "defaults/bootstrap_apps.sh"
+sh "defaults/bootstrap_appstore.sh"
+sh "defaults/bootstrap_dbug.sh"
+sh "defaults/bootstrap_dock.sh"
+sh "defaults/bootstrap_finder.sh"
+sh "defaults/bootstrap_graphics.sh"
+sh "defaults/bootstrap_keyboard.sh"
+sh "defaults/bootstrap_mail.sh"
+sh "defaults/bootstrap_mouse.sh"
+sh "defaults/bootstrap_safari.sh"
+sh "defaults/bootstrap_spaces.sh"
+sh "defaults/bootstrap_system.sh"
+sh "defaults/bootstrap_textedit.sh"
+sh "defaults/bootstrap_tmachine.sh"
+sh "defaults/bootstrap_trackpad.sh"
 
 # Restart applications and services
 for app in "Address Book" "Calendar" "Contacts" "Dock" "Finder" "Mail" \
