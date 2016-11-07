26bae03a7a0acb72216a1c2fce862cec45df0b3e
Patrick Mullins <pmullins11@mac.com>
2016-11-07 16:47:07 -0500

    Renamed files

---
+++ boot_macos.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# BASH script used to bootstrap a macOS machine.
+# Author: Patrick Mullins (pmullins11@mac.com)
+# Last update: October 29th, 2016
+
+# Ask for the administrator password up front
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx_setup` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Clear the screen
+clear
+
+# Included Modules
+sh "defaults/bootstrap_apps.sh"
+sh "defaults/bootstrap_appstore.sh"
+sh "defaults/bootstrap_dbug.sh"
+sh "defaults/bootstrap_dock.sh"
+sh "defaults/bootstrap_finder.sh"
+sh "defaults/bootstrap_graphics.sh"
+sh "defaults/bootstrap_keyboard.sh"
+sh "defaults/bootstrap_mail.sh"
+sh "defaults/bootstrap_safari.sh"
+sh "defaults/bootstrap_spaces.sh"
+sh "defaults/bootstrap_system.sh"
+sh "defaults/bootstrap_textedit.sh"
+sh "defaults/bootstrap_tmachine.sh"
+sh "defaults/bootstrap_trackpad.sh"
+
+# Restart applications and services
+for app in "Address Book" "Calendar" "Contacts" "Dock" "Finder" "Mail" \
+        "Messages" "Safari" "SystemUIServer" "Terminal" "SystemUIServer" \
+        "iCal"; do
+        killall "${app}" > /dev/null 2>&1
+done
