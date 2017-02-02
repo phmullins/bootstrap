d734fcc89ff2bfad603a5bd442127f133469ddf2
Patrick Mullins <pmullins11@mac.com>
2017-02-02 14:34:24 -0600

    boot_mas.sh

--- boot_mas.sh
+++ boot_mas.sh
@@ -19,26 +19,26 @@ if [[ $(mas account) == "Not signed in" ]]; then
 	mas signin com.pmullins11@mac.com
 fi
 
-# List if apps to be installed. Use MAS to determine app numbers.
+# List of apps to be installed. Use MAS to determine app numbers.
 declare -a apps=(
-        420212497  # Byword (2.7)
-				422304217  # Day One Classic (1.10.6)
-        455970963  # Disk Doctor (3.6)
-				686428787  # Duplicate Detective
-        490152466  # iBooks Author (2.5)
-				524141863  # Jump Desktop
-        1142151959 # JustFocus (1.3.3)
-				409183694  # Keynote (7.0.5)
-				557168941  # Tweetbot (2.4.4)
-				624884154  # Lingo (2.0.5)
-				890031187  # Marked 2 (2.5.6)
-				409203825  # Numbers (4.0.5)
-        409201541  # Pages (6.0.5)
-        924891282  # Pixave (2.1.4)
-        407963104  # Pixelmator (3.5.1)
-				883878097  # Server (5.2)
-				443126292  # TrashMe (2.1.14)
-        497799835  # Xcode (8.1)
+	420212497  # Byword (2.7)
+	422304217  # Day One Classic (1.10.6)
+	455970963  # Disk Doctor (3.6)
+	686428787  # Duplicate Detective
+	490152466  # iBooks Author (2.5)
+	524141863  # Jump Desktop
+	1142151959 # JustFocus (1.3.3)
+	409183694  # Keynote (7.0.5)
+	557168941  # Tweetbot (2.4.4)
+	624884154  # Lingo (2.0.5)
+	890031187  # Marked 2 (2.5.6)
+	409203825  # Numbers (4.0.5)
+	409201541  # Pages (6.0.5)
+	924891282  # Pixave (2.1.4)
+	407963104  # Pixelmator (3.5.1)
+	883878097  # Server (5.2)
+	443126292  # TrashMe (2.1.14)
+	497799835  # Xcode (8.1)
 )
 
 mas install --force "${apps[@]}"
